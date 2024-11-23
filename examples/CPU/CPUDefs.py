e1cebc4dbff5dd6208a6e06708f08e2cf080690c
andrea <andrea.luzzati@gmail.com>
2024-11-24 00:30:01 +0100

    added cpu ISA documentation to CPUDefs

--- examples/CPU/CPUDefs.py
+++ examples/CPU/CPUDefs.py
@@ -9,30 +9,74 @@ from logisim import Net,Vector,Cell
 
 # An instruction is 32 bit long
 # In an instruction, only the opcode is fixed size (8 bits)
-# The remaining 24 bits depend on the instruction
+# and it is always located at the lowest significant bits.
+# Also, when an instruction uses a register immediate word, it
+# is always at [14:29] in the instruction.
+# The remaining bits depend on the instruction.
+# It follows the instruction set architecture.
 
-# In general:
-# the suffix D means memory[immediate]
-# the suffix M means memory[REG]
 
-# In MOV instructions:
-# the syntax is MOV TYPE | DST | SRC
-# MOVRR REG(2bit) | REG(2bit)                   OK
-# MOVRI REG(2bit) | IMMEDIATE(16bit)            OK
-# MOVRD REG(2bit) | IMMEDIATE ADDRESS(16 bit)   OK
-# MOVRM REG(2bit) | REG ADDRESS(2 bit)          OK
-# MOVMR REG ADDRESS(2 bit) | REG(2 bit)         TEST
-# MOVMI REG ADDRESS(2 BIT) | IMMEDIATE(16 bit)  TEST
-# MOVDR IMMEDIATE ADDRESS (16 bit) | REG(2 bit) TEST
+#     NOP  | 
+#    [0:7] | [8:31]
+#   -------|---------
+#    0x00  | 0x000000
+#
+# No operation
 
-# in ALU instructions:
-# the syntax is ADD DST A B
-# ADDRR REG(2bit) | REG(2bit) | REG(2bit)
-# ADDRI REG(2bit) | REG(2bit) | Immediate(16 bit)
+#     MOV  |   DST    |   SRC    |   IM    | REGDST
+#    [0:7] |  [8:10]  | [11:13]  | [14:29] | [30:31]
+#   -------|----------|----------|---------|---------
+#    0x01  | A  = 000 | A  = 000 |         | A = 00
+#          | B  = 001 | B  = 001 |         | B = 01
+#          | C  = 010 | C  = 010 |         | C = 10
+#          | D  = 011 | D  = 011 |         | D = 11
+#          | pR = 100 | pR = 100 |         |
+#          | pI = 101 | pI = 101 |         |
+#          |          | IM = 110 |         |
+#
+# Moves a word from SRC to DST.
+# pR means an access to memory at the location specified by
+# REGDST. pI means an access to memory at the location specified by
+# the 16 bit Immediate in the instruction.
+# An instruction cannot access to memory both for SRC and DST.
 
+#     ADD  |    A     |    B    |   IM    | REGDST
+#    [0:7] | [8:10]   | [11:13] | [14:29] | [30:31]
+#   -------|----------|---------|---------|---------
+#    0x02  | A  = 000 | A = 000 |         | A = 00
+#          | B  = 001 | B = 001 |         | B = 01
+#          | C  = 010 | C = 010 |         | C = 10
+#          | D  = 011 | D = 011 |         | D = 11
+#          | IM = 100 |         |         |
+#  
+# Adds two words and store the result to a register.
+# operaend A can be a register or the instruction immediate.
+# operand B must be a register.
+
+#     JMP  |   CND     |  OP      |   IM    |
+#    [0:7] |  [8:10]   | [11:13]  | [14:29] | [30:31]
+#   -------|-----------|----------|---------|---------
+#    0x03  | UNC = 000 | A  = 000 |         | 0b00
+#          | EQ  = 001 | B  = 001 |         |
+#          | NEQ = 010 | C  = 010 |         |
+#          | GRE = 011 | D  = 011 |         |
+#          | LOW = 100 | IM = 100 |         |
+#          
+# Branches to a new instruction memory address; a condition can be applied
+# by using the CND field. Conditions are referred to the status registers
+# that is updated after mathemeatical operations (ADD,SUB,CMP).
+# UNC - unconditional jump
+# EQ  - jump if equal
+# NEQ - jump if not equal
+# GRE - jump if greater
+# LOW - jump if lower
 
 INSTRUCTION_LENGTH = 32
 INSTRUCTION_OPCODE_LENGTH = 8
+
+class AutoEnum(int,Enum):
+    def _generate_next_value_(name, start, count, last_values):
+        return count
 
 class MOVOP(int,Enum):
     def _generate_next_value_(name, start, count, last_values):
@@ -54,17 +98,30 @@ class ADDOP(int,Enum):
     D = auto()
     IMMEDIATE = auto()
 
+class JMPCND(AutoEnum):
+    UNC = auto() # Unconditionate jump
+    EQ  = auto() # Jump if equal
+    NEQ = auto() # Jump if not equal
+    GRE = auto() # Jump if greater
+    LOW = auto() # Jump if lower
+
+
+
 class OPCODE(int,Enum):
     def _generate_next_value_(name, start, count, last_values):
         return count
-    NOP   = auto() # NOP - do nothing
-    MOV   = auto() # MOV - move a word from/to registers,immediate and memory
-    ADD   = auto() # ADD - sum two words
-    SUB   = auto() # SUB - subtract two words
-    SFL   = auto() # SFL - Shift left (arithmetic or logic) a register
-    SFR   = auto() # SFR - Shift right (arithmetic or logic) a register
+    NOP   = auto() # NOP  - do nothing
+    MOV   = auto() # MOV  - move a word from/to registers,immediate and memory
+    ADD   = auto() # ADD  - sum two words
+    JMP   = auto() # JMP  - Jump to another location
+    SUB   = auto() # SUB  - subtract two words
+    SFL   = auto() # SFL  - Shift left (arithmetic or logic) a register
+    SFR   = auto() # SFR  - Shift right (arithmetic or logic) a register
     ROTL  = auto() # ROTL - Rotate lef a register
     ROTR  = auto() # ROTR - Rotate right a register
+    CMP   = auto() # CMP  - Compare (performs a sub without a destination)
+    
+
 
     COUNT = auto() # Number of defined opcodes
 
